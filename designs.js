0f3b873edde387cb2618fdab8a959f66e7cea797
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-04-05 19:32:37 +0100

    adds colock to clicked table cell

--- designs.js
+++ designs.js
@@ -9,18 +9,32 @@ $('#sizePicker').submit(function (e) {
     height = $('#inputHeight').val(); 
     width = $('#inputWidth').val(); 
     makeGrid(height, width);
-    console.log('Height: ' + height + ' and width: ' + width);
 });
 
 function makeGrid(x, y) {
 
     // Your code goes here!
-    $('tr').remove();//clears existing table
+    $('tr').remove();//clears existing content of the table
     for (var i = 1; i <= x; i++) {
-        $('#pixelCanvas').append('<tr id =table' + i + '></tr>');//adds row to the pixel canvas
+        $('#pixelCanvas').append('<tr id =table' + i + '></tr>');//adds row(s) to the pixel canvas
         for (var j = 1; j <= y; j++) {
             $('#table' + i).append('<td></td>');//adds columns to the rows created in the pixel canvas
         }
 
     }
+
+    $('td').click(function addColor() {
+        color = $('#colorPicker').val();
+        /*
+        if clicked cell is styled, clear.
+        else, style the cell with color picked with the color picker
+         */
+        if ($(this).attr('style')) {
+            $(this).removeAttr('style')            
+        } else {
+            $(this).attr('style', 'background-Color:' + color);
+            
+        }
+        
+    });
 }
